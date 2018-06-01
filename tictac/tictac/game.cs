48195abb10b56657b955f74f0be186e86c42d95d
Антропов Павел <sidexrus@yandex.ru>
2018-06-01 14:38:57 +0700

    Test - right column winner (passed)

--- tictac/tictac/game.cs
+++ tictac/tictac/game.cs
@@ -21,6 +21,7 @@ namespace tictac
             if ((result[0] == result[1]) && (result[0] == result[2]) && result[0] != 0) return 1;
             if ((result[0] == result[3]) && (result[0] == result[6]) && result[0] != 0) return 1;
             if ((result[1] == result[4]) && (result[1] == result[7]) && result[1] != 0) return 1;
+            if ((result[2] == result[5]) && (result[2] == result[8]) && result[2] != 0) return 1;
             return 2;
         }
     }
