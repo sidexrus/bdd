97273fddd8d5f292b1ef11289fed02e2cda9a6eb
Антропов Павел <sidexrus@yandex.ru>
2018-06-01 14:32:02 +0700

    Test - left column winner (passed)

--- tictac/tictac/game.cs
+++ tictac/tictac/game.cs
@@ -19,6 +19,7 @@ namespace tictac
         public int checkresult(List<int> result)
         {
             if ((result[0] == result[1]) && (result[0] == result[2]) && result[0] != 0) return 1;
+            if ((result[0] == result[3]) && (result[0] == result[6]) && result[0] != 0) return 1;
             return 2;
         }
     }
